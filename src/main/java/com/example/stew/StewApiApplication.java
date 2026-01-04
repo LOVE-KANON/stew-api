e5eeb76e1a62717beab660ab615f80acfe0adb48
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-04 13:29:01 +0900

    Initial Commit

---
+++ src/main/java/com/example/stew/StewApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StewApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StewApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/stew/StewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StewApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
