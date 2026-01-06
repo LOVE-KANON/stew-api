154c81d42b412e7e8140ea892c615ae8021bcd0b
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-07 00:59:20 +0900

    【develop】DB関連設定 + 接続確認

--- src/main/java/com/example/stew/core/api/usecase/service/LoginService.java
+++ src/main/java/com/example/stew/core/api/usecase/service/LoginService.java
@@ -1,17 +1,29 @@
 package com.example.stew.core.api.usecase.service;
 
-import org.springframework.stereotype.Service;
-
+import com.example.stew.core.api.resource.entity.CoreUserEntity;
+import com.example.stew.core.api.resource.mapper.CoreUserMapper;
 import com.example.stew.core.api.usecase.controller.dto.login.LoginRequest;
 import com.example.stew.core.api.usecase.controller.dto.login.LoginResponse;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class LoginService {
 
+    private final CoreUserMapper coreUserMapper;
+
     public LoginResponse login(LoginRequest request) {
+
+        // ユーザ情報取得
+        CoreUserEntity user = coreUserMapper.findByPk(CoreUserEntity.builder().userId(request.getUserId()).build());
+
+        if (user == null || !user.getPassword().equals(request.getPassword())) {
+            return new LoginResponse(false, "ログイン情報が不正です");
+        }
+
+        // TODO: Redis にセッション or JWT 発行
         return new LoginResponse(true, "ログイン成功");
     }
 }
