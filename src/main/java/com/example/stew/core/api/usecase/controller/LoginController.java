956867cdc96e11c7a80c19cbfae3e8a6fcca07f1
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-07 00:10:20 +0900

    LoginService作成(ひとまず何も処理なし)

--- src/main/java/com/example/stew/core/api/usecase/controller/LoginController.java
+++ src/main/java/com/example/stew/core/api/usecase/controller/LoginController.java
@@ -1,22 +1,21 @@
 package com.example.stew.core.api.usecase.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 import com.example.stew.core.api.usecase.controller.dto.login.LoginRequest;
 import com.example.stew.core.api.usecase.controller.dto.login.LoginResponse;
-
-import lombok.RequiredArgsConstructor;
+import com.example.stew.core.api.usecase.service.LoginService;
 
 @RestController
 @RequestMapping("/api/core/usecase")
 @RequiredArgsConstructor
 public class LoginController {
 
+    private final LoginService loginService;
+
     @PostMapping("/login")
     public LoginResponse login(@RequestBody LoginRequest request) {
-        return new LoginResponse();
+        return loginService.login(request);
     }
 }
---
+++ src/main/java/com/example/stew/core/api/usecase/service/LoginService.java
@@ -0,0 +1,17 @@
+package com.example.stew.core.api.usecase.service;
+
+import org.springframework.stereotype.Service;
+
+import com.example.stew.core.api.usecase.controller.dto.login.LoginRequest;
+import com.example.stew.core.api.usecase.controller.dto.login.LoginResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class LoginService {
+
+    public LoginResponse login(LoginRequest request) {
+        return new LoginResponse();
+    }
+}
