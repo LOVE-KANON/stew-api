47a622be991fa3c09ddac107f50c7479b4a14e4c
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-30 03:16:25 +0900

    【develop】DateTimeUtilsのメソッド追加

--- src/main/java/com/example/stew/core/utils/DateTimeUtils.java
+++ src/main/java/com/example/stew/core/utils/DateTimeUtils.java
@@ -22,4 +22,36 @@ public final class DateTimeUtils {
             return null;
         }
     }
+
+    /**
+     * 型変換(日時⇒日時文字列)
+     * 変換失敗時はnullをリターン
+     * @param dt 対象日時
+     * @param formatter 日時フォーマット
+     * @return String
+     */
+    public static String toString(LocalDateTime dt, DateTimeFormatter formatter) {
+        try {
+            return dt.format(formatter);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    /**
+     * 日時文字列のフォーマット変換(日時文字列⇒日時文字列)
+     * 変換失敗時はnullをリターン
+     * @param str 対象文字列
+     * @param srcFormatter 対象文字列の日時フォーマット(変換前)
+     * @param destFormatter 対象文字列の日時フォーマット(変換後)
+     * @return String
+     */
+    public static String convertDateTimeFormat(String str, DateTimeFormatter srcFormatter, DateTimeFormatter destFormatter) {
+        try {
+            LocalDateTime localDateTime = toLocalDateTime(str, srcFormatter);
+            return toString(localDateTime, destFormatter);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 }
