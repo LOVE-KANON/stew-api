9cfefdafe1d98cf95b90c619bc08c57d9d479ee2
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-30 03:14:58 +0900

    【develop】DateUtilのフォーマット指定で扱うクラスを変更(SimpleDateFormat⇒DateTimeFormatter)

--- src/main/java/com/example/stew/core/utils/DateUtils.java
+++ src/main/java/com/example/stew/core/utils/DateUtils.java
@@ -1,30 +1,23 @@
 package com.example.stew.core.utils;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 /**
  * 日付ユーティリティクラス
  */
 public final class DateUtils {
 
-    public static final String FORMAT_HYPHEN_YMD_COLON_HMS = "yyyy-MM-dd HH:mm:ss";
-    public static final String FORMAT_SLASH_YMD_COLON_HMS = "yyyy/MM/dd HH:mm:ss";
-
     /**
      * 型変換(文字列⇒日付)
      * 変換失敗時はnullをリターン
      * @param str 対象文字列
-     * @param format 日付フォーマット
+     * @param formatter 日付フォーマット
      * @return LocalDateTime
      */
-    public static LocalDateTime toLocalDateTime(String str, String format) {
+    public static LocalDateTime toLocalDateTime(String str, DateTimeFormatter formatter) {
         try {
-            SimpleDateFormat sdf = new SimpleDateFormat(format);
-            Date formatDate = sdf.parse(str);
-            return LocalDateTime.ofInstant(formatDate.toInstant(), ZoneId.systemDefault());
+            return LocalDateTime.parse(str, formatter);
         } catch (Exception e) {
             return null;
         }
