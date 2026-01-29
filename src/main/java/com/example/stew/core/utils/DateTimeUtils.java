e0d5bf8f370bcf6e2cdbedc8dbe27b140f392513
HikaruMaruyama <Hikaru.Maruyama@example.com>
2026-01-30 03:15:30 +0900

    【develop】DateUtils⇒DateTimeUtilsとし、中の単語も「日付⇒日時」に変更

--- src/main/java/com/example/stew/core/utils/DateTimeUtils.java
+++ src/main/java/com/example/stew/core/utils/DateTimeUtils.java
@@ -4,15 +4,15 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 /**
- * 日付ユーティリティクラス
+ * 日時ユーティリティクラス
  */
-public final class DateUtils {
+public final class DateTimeUtils {
 
     /**
-     * 型変換(文字列⇒日付)
+     * 型変換(日時文字列⇒日時)
      * 変換失敗時はnullをリターン
      * @param str 対象文字列
-     * @param formatter 日付フォーマット
+     * @param formatter 日時フォーマット
      * @return LocalDateTime
      */
     public static LocalDateTime toLocalDateTime(String str, DateTimeFormatter formatter) {
